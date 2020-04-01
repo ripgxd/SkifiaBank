0932b51f9bef69ced6945dddd3f42da55dcca91b
ripgxd <60903831+ripgxd@users.noreply.github.com>
2020-04-01 18:43:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     TOKEN: "1132829149:AAGtDNa2TPnA9-J5Kz4yh9J1wAT80ZEoUm4",
-    DB_URL: "mongodb://localhost:27017/skifia_bank_users"
+    DB_URL: "mongodb://localhost:27017/skifiabank"
 }
